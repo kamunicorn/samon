af9d0e8e7ee19357e2d5c85e6a69deaf07098756
vera <vera.kamunicorn@gmail.com>
2019-06-20 21:15:40 +1200

    Переделала каталог под php - массив с информацией, добавила соответствующие картинки

--- catalog.php
+++ catalog.php
@@ -1,3 +1,33 @@
+<?php
+$categories = [
+    [
+        'name' => 'Тротуарная плитка',
+        'code' => 'trotuar',
+        'image' => 'trotuar.jpg'
+    ], [
+        'name' => 'Бетонопаркет',
+        'code' => 'parket',
+        'image' => 'parket-2.jpg'
+    ], [
+        'name' => 'Облицовочный камень',
+        'code' => 'outer-stone',
+        'image' => 'facing-stone.jpg'
+    ], [
+        'name' => 'Ступени накладные, бордюры, водостоки',
+        'code' => 'different',
+        'image' => 'different.jpg'
+    ], [
+        'name' => 'Архитектурно-ландшафтные формы',
+        'code' => 'decor',
+        'image' => 'decor-2.jpg'
+    ], [
+        'name' => 'Цветники',
+        'code' => 'flower',
+        'image' => 'flower.jpg'
+    ]
+];
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -14,12 +44,11 @@
         <h3 class="section__title">Каталог</h3>
         <p class="catalog__subtitle">Выберите нужный тип продукта</p>
         <div class="container row">
-            <div class="catalog__item" style="background-image:url(img/catalog/catalog-1.jpg)"><a href="/category.html" class="catalog__item-title">Название продукции</a></div>
-            <div class="catalog__item" style="background-image:url(img/catalog/catalog-2.jpg)"><a href="/category.html" class="catalog__item-title">Название продукции</a></div>
-            <div class="catalog__item" style="background-image:url(img/catalog/catalog-3.jpg)"><a href="/product.html" class="catalog__item-title">Название продукции</a></div>
-            <div class="catalog__item" style="background-image:url(img/catalog/catalog-4.jpg)"><a href="/category.html" class="catalog__item-title">Название продукции</a></div>
-            <div class="catalog__item" style="background-image:url(img/catalog/catalog-5.jpg)"><a href="/category.html" class="catalog__item-title">Название продукции</a></div>
-            <div class="catalog__item" style="background-image:url(img/catalog/catalog-6.jpg)"><a href="/category.html" class="catalog__item-title">Название продукции</a></div>
+            <?php
+            foreach ($categories as $category) { ?>
+                <div class="catalog__item" style="background-image:url('img/catalog/<?= $category['image']?>')"><a href="/category.php?name=<?= $category['code']?>" class="catalog__item-title"><?= $category['name']?></a></div>
+            <? } ?>
+
         </div>
     </section>
 
