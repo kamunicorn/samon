d982b159dee446a94eefcdbd64892a2a27be7ab1
vera <vera.kamunicorn@gmail.com>
2019-06-23 22:20:13 +1200

    Ура, слайдер работает!

--- js/gallery.js
+++ js/gallery.js
@@ -6,27 +6,35 @@ document.addEventListener('DOMContentLoaded', function() {
         btnPrev = document.querySelector('.slider__buttons.prev'),
         btnNext = document.querySelector('.slider__buttons.next'),
         btnClose = document.querySelector('.slider__close'),
-        counter = 0;
+        index = 0;
     
     galleryBox.addEventListener('click', function(e) {
         console.log(e.target);
         if (e.target.classList.contains('gallery__item')) {
             slider.style.display = 'flex';
             document.body.style.overflow = 'hidden';
-            let oldContent = sliderContent.firstElementChild,
-                newContent = document.createElement('img');
-            newContent.src = e.target.querySelector('img').getAttribute('src');
-            sliderContent.replaceChild(newContent, oldContent);
+            index = Array.prototype.indexOf.call(galleryItems, e.target);
+            changeSlide(index);
+            console.log(index);
         }
     });
 
     btnNext.addEventListener('click', function(){
-
+        index = (index == galleryItems.length - 1) ? 0 : index + 1;
+        changeSlide(index);        
     });
 
     btnPrev.addEventListener('click', function(){
-        
+        index = (index == 0) ? galleryItems.length - 1 : index - 1;
+        changeSlide(index);        
     });
+
+    function changeSlide(index) {
+        let newContent = document.createElement('img');
+        newContent.src = galleryItems[index].querySelector('img').getAttribute('src');
+        sliderContent.textContent = '';
+        sliderContent.appendChild(newContent);
+    }
 
     slider.addEventListener('click', function(e){
         if (e.target == this || e.target == btnClose) {
