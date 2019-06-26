0f09947c644ca627c51311c3810b7169f1081edc
vera <vera.kamunicorn@gmail.com>
2019-06-26 16:18:55 +1200

    Добавила в хед метатеги и прочее

--- php/head.php
+++ php/head.php
@@ -1,5 +1,26 @@
-<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-<link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon">
+<meta name="author" content="Вера Перелыгина" />
+
+<!-- Заголовок страницы -->
 <title><?= ($title) ? $title . ' | САМОН' : 'САМОН'; ?></title>
+
+<!-- Мета-теги для SEO и вообще -->
+<meta name="title" content="КПК «САМОН»" />
+<meta name="description" content="Изготовление изделий из бетона на Камчатке по технологии «Мрамор из бетона»" />
+<meta name="keywords" content="изготовление изделий из бетона на Камчатке, изделия из бетона на Камчатке, кевларобетон на Камчатке, мрамор из бетона на Камчатке, тротуарная плитка на Камчатке, бетонопаркет на Камчатке, облицовочный камень на Камчатке, цветники на Камчатке, накладные ступени на Камчатке, бордюры на Камчатке, водостоки на Камчатке" />
+<meta name="robots" content="index,follow" />
+<meta name="reply-to" content="kpk-samon@yandex.ru" />
+
+<!-- Теги для расшаривания в соцсети -->
+<meta property="og:url" content="//kpk-samon.ru" />
+<meta property="og:title" content="КПК «САМОН»" />
+<meta property="og:image" content="//kpk-samon.ru/img/favicon.png" />
+<meta property="og:description" content="Изготовление изделий из бетона на Камчатке по технологии «Мрамор из бетона»" />
+
+<!-- Иконки на вкладке -->
+<link rel="apple-touch-icon" href="/img/favicon.png" type="image/x-icon" />
+<link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon">
+<!-- Цвет браузерной вкладки -->
+<meta name="theme-color" content="#457FD7"/>
